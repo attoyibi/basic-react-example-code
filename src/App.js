d60922fc02726f4850966f6ce5ce8d47cbd92de2
attoyibi <muchson@alterra.id>
2022-04-04 18:50:49 +0700

    aysc pakek axios and loading spinner bostrab

--- src/App.js
+++ src/App.js
@@ -2,28 +2,44 @@ import logo from "./logo.svg";
 import "./App.css";
 import react, { useEffect, useState } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Spinner from "react-bootstrap/Spinner";
 
 function App() {
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    console.log("fetching data");
-    //letak dari kita pasang axios
-    axios
-      .get(" https://pokeapi.co/api/v2/pokemon/ditto")
-      .then((res) => {
-        console.log(res.data.abilities[0].ability.name);
-        setData(res.data.abilities[0].ability.name);
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log("Data Pokemon gak ketemu");
-        setError("Data Pokemon gak ketemu");
-      });
+    async function fetchData() {
+      console.log("fetching data");
+      //letak dari kita pasang axios
+
+      try {
+        const data = await axios
+          .get(" https://pokeapi.co/api/v2/pokemon/ditto")
+          .then((res) => {
+            console.log(res.data.abilities[0].ability.name);
+            setData(res.data.abilities[0].ability.name);
+          })
+          .catch((err) => {
+            console.log(err);
+            console.log("Data Pokemon gak ketemu");
+            setError("Data Pokemon gak ketemu");
+          });
+        //set time out untuk menunjukkan proses loading, tapi kalau datanya banyak gak perlu di set time out
+        setTimeout(() => {
+          setLoading(true);
+        }, 1000);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchData();
   }, []);
   return (
     <div className="App">
-      <h3>{data}</h3>
+      {loading ? data : <Spinner animation="border" />}
+
       <h3>{error}</h3>
     </div>
   );
