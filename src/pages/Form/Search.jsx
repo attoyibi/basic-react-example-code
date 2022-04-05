7cc745e2077aca70858156f96833f18f8f9c6546
attoyibi <muchson@alterra.id>
2022-04-05 21:39:08 +0700

    code

--- src/pages/Form/Search.jsx
+++ src/pages/Form/Search.jsx
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
-import axios from 'axios';
- 
-const URL = 'http://hn.algolia.com/api/v1/search';
- 
+import React, { useState } from "react";
+import axios from "axios";
+
+const URL = "http://hn.algolia.com/api/v1/search";
+
 export default function Search() {
   const [stories, setStories] = useState([]);
   const [error, setError] = useState(null);
- 
+
   async function handleFetch(event) {
     let result;
- 
+
     try {
       result = await axios.get(`${URL}?query=React`);
       setStories(result.data.hits);
@@ -17,15 +17,15 @@ export default function Search() {
       setError(error);
     }
   }
- 
+
   return (
     <div>
       <button type="button" onClick={handleFetch}>
         Cari Cerita React
       </button>
- 
+
       {error && <span>Ada yang error ...</span>}
- 
+
       <ul>
         {stories.map((story) => (
           <li key={story.objectID}>
