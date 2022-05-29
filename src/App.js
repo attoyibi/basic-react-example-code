6166cda6705549722f90a0f1a399a42b3579fb94
alterra-muchson <muchson@alterra.id>
2022-05-29 14:39:53 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,45 +5,14 @@ import Hoc from './HOC';
 import StockList from './StockList';
 import UserList from './UserList';
 
-const StocksData = [
-  {
-      id: 1,
-      name: 'TCS'
-        
-  },
-  {
-      id: 2,
-      name: 'Infosys'
-  },
-  {
-      id: 3,
-      name: 'Reliance'
-  }
-];
-const UsersData = [
-  {
-      id: 1,
-      name: 'Krunal'
-        
-  },
-  {
-      id: 2,
-      name: 'Ankit'
-  },
-  {
-      id: 3,
-      name: 'Rushabh'
-  }
-];
+
 
 const Stocks = Hoc(
   StockList,
-  StocksData
 );
 
 const Users = Hoc(
   UserList,
-  UsersData
 );
 
 class App extends Component {
