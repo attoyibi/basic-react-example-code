73f6b75d9eb8d65dbc9d492219ee7ddd2029dd70
alterra-muchson <52569071+muhammad-muchson@users.noreply.github.com>
2022-07-31 14:03:45 +0700

    make basic react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, {useState, useEffect} from 'react'
-import style from './App.module.css' 
-import Header from './header.jsx'
-import './App.css' 
+//tempat untuk import semua component, hook, dan logic component
+import React from 'react'
 
 export default function App() {
-  const [nama, setNama] = useState("muchson");
-  const handleClick = () =>{
-    setNama("hai")
-  }
-  // didmound di dalam
-  useEffect(()=>{
-    console.log("use Effect didMount")
-  },[])
+//tempat logic dalam component digunakan
 
-  // let nama = "muchson";
-  let status = true;
-  return (
-    <>
-    <Header data={nama} dataStatus={status}/>
-    <div onClick={handleClick} style={status ? {border: '1px solid green '}: {border: '1px solid red '}} className={style.yellow}>{nama}</div>
-    </>
+    return (
+    // semua hal dalam return akan di render
+    <div>App</div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './AppCopy';
-import AppClass from 'AppClass';
+// import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import client from './apollo-client';
-import { ApolloProvider } from '@apollo/client';
 
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
     <React.StrictMode>
-      <App></App>
-      {/* <App data="data dari index" /> */}
-      {/* <AppClass data="data dari index" /> */}
-    </React.StrictMode>
-  </ApolloProvider>,
+      <App/>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
