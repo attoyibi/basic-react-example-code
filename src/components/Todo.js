84c2befadd231861b09cc16931650d5dfaa0c9be
alterra-muchson <52569071+muhammad-muchson@users.noreply.github.com>
2022-08-21 15:39:29 +0700

    remove graphwl todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,8 +5,10 @@ export default function Todo({
   onClickItem,
   onDeleteItem,
 }) {
+  console.log("tile", title);
+  console.log("onClickItem", onClickItem);
   return (
-    <li className={checked ? 'done todo-item' : 'todo-item'} data-key={id}>
+    <li className={checked ? "done todo-item" : "todo-item"} data-key={id}>
       <input onChange={onClickItem} id={id} type="checkbox" />
       <label htmlFor={id} className="tick js-tick"></label>
       <span>{title}</span>
