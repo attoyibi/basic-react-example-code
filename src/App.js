583f88b87b1e154569259c096bea8dd71d008742
alterra-muchson <52569071+muhammad-muchson@users.noreply.github.com>
2022-09-04 17:59:04 +0700

    basoc auth0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 //tempat untuk import semua component, hook, dan logic component
-import React from 'react'
+import React from "react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function App() {
-//tempat logic dalam component digunakan
+  //tempat logic dalam component digunakan
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-    return (
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+  return (
     // semua hal dalam return akan di render
-    <div>App</div>
-  )
+    <div>
+      {/* membuat tombol login */}
+      <LoginButton />
+      {/* membuat tombol logut */}
+      <LogoutButton />
+      {/* jika telah melakukan login maka data dibawah akan di tambilkan, tapi jika belum maka isAuthenticated bernilai false, dan data tidak akan ditampilak */}
+      {isAuthenticated && (
+        <div>
+          <img src={user?.picture} alt={user?.name} />
+          <h2>{user?.name}</h2>
+          <p>{user?.email}</p>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>,
-  document.getElementById('root')
+  <Auth0Provider
+    domain="dev-4o8ewg0o.us.auth0.com"
+    clientId="JNRrYasvIVC1ssme1myjFykmuVVOFPu1"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
